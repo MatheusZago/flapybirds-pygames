7d1adec9ef253fc8fc40a4214367abc4481110fd
Matheus <matheuszago134@gmail.com>
2025-03-21 18:03:53 -0300

    Add background

--- flappy.py
+++ flappy.py
@@ -1,14 +1,21 @@
 import pygame
 from pygame.locals import *
+import os
 
-#Tamanho da tela
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 800
+#Pegando onde está as imagens
+BASE_DIR = os.path.dirname(__file__)  # Diretório do script atual
+BACKGROUND_IMAGE_PATH = os.path.join(BASE_DIR, 'assets', 'sprites', 'background-day.png')
+BACKGROUND = pygame.image.load(BACKGROUND_IMAGE_PATH)
+BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
 #chamando a função de iniciar
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
 
 while True:
     #Evento é qualquer ação que pode realizar dentro do jogo
@@ -17,6 +24,9 @@ while True:
         if event.type == QUIT:
             pygame.QUIT
             exit()
+
+        #Colocando o background, ele começa no canto superior esquerto da
+        screen.blit(BACKGROUND, (0, 0))
 
     #Depois de cada laço ele atualiza pra mudar oq precisa ser mudado
     pygame.display.update()
